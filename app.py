a20934ca08e2d6d5702f258c53b55363877415ae
seitzfabian-svg <seitz.fabian@outlook.com>
2026-02-09 13:31:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ MSG_AGENCY = "00"
 
 st.set_page_config(page_title="DTA Experte – Ausbaustufe 1 (Testdatei Generator)", layout="wide")
 st.title("🚀 DTA Experte – Ausbaustufe 1 (Testdatei Generator) ")
-st.caption("AUFN/ENTL/RECH sind synthetisch befüllt und konsistent (gleicher Fall). Alle anderen Typen werden leer erzeugt (nur FKT).")
+st.caption(" TP4a-Dateien können erzeugt werden: AUFN/ENTL/RECH sind synthetisch befüllt und konsistent (gleicher Fall). 
+Alle anderen Nachrichtentypen werden leer erzeugt (nur FKT).")
 st.divider()
 
 # -----------------------------
